4028e0bbf82612043535820bd3054a27ceef277a
fissalalsharef <fissalalsharef@gmail.com>
2018-03-21 16:50:12 +0300

    initial

--- CBS/settings.py
+++ CBS/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'CBS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'Scanning_MSGs/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
